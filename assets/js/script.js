c7552a1d25cf88934b98be0441ec9b79e8134501
JonathanKim424 <vizard424@yahoo.com>
2022-03-09 19:41:18 -0700

    optional features: added task audit to grey out tasks not saved & added timer audit to update hour and date every 30 min

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,18 @@ var saveTask = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var auditTask = function(taskID) {
+    var rowID = $("#" + taskID).closest(".row").attr("id");
+    var currTxt = $("#" + taskID).val();
+    var saveTxt = tasks[rowID].text;
+    if (currTxt != saveTxt) {
+        $("#" + taskID).css("color", "grey");
+    }
+    if (currTxt === saveTxt) {
+        $("#" + taskID).css("color", "black");
+    }
+};
+
 $("button").on("click", function() {
     var rowID = $(this).closest(".row").attr("id");
     var taskID = $("#" + rowID).children("textarea").attr("id");
@@ -49,7 +61,17 @@ $("button").on("click", function() {
         text: taskText
     };
     saveTask();
-})
+    auditTask(taskID);
+});
+
+$("textarea").on("blur", function() {
+    var taskID = $(this).attr("id");
+    auditTask(taskID);
+});
+
+setInterval(function() {
+    currentDate();
+}, (1000 * 60) * 30);
 
 currentDate();
 loadTasks();
